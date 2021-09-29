e80b0c4fe22de968db0489e2dc0ebcebda007cc1
wangxf <wangxf108@gmail.com>
2021-09-30 00:49:31 +0900

    as

---
+++ books/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path 
+from . import views 
+
+
+
+app_name = 'books'
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
+    path('authors/', views.AuthorListView.as_view(), name='list_authors'),
+    path('authors/add/', views.AuthorCreateView.as_view(), name='add_author'),
+    path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='author_detail'),
+]
---
+++ books/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.views.generic import (TemplateView, ListView, CreateView, DetailView)
+from .models import Author 
+from django.contrib import messages
+
+
+class HomeView(TemplateView):
+    template_name = 'books/home.html'
+
+class AuthorListView(ListView):
+    model = Author
+    template_name = 'books/author_list.html'
+
+# createview 从表单拿到数据，并且将数据加入到模型中
+class AuthorCreateView(CreateView):
+    model = Author
+    template_name = 'books/author_create.html'
+    # 此处添加field，相当于创建了一个form，另外form也可以单独拿出去，创建一个form.py
+    fields = ['name',]
+
+    def form_valid(self, form):
+
+        # 成功添加author，然后返回成功添加信息，调用的是父类的form_valid()
+        messages.add_message(
+            self.request, 
+            messages.SUCCESS,
+            'The author has been added'
+        )
+        return super().form_valid(form)
+
+
+class AuthorDetailView(DetailView):
+    model = Author
+    template_name = "books/author_detail.html"
